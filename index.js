5e8a92954d93b7538e98c7be69d9bfd3d686bd20
WaterMan-Joy <nietzsche21st@gmail.com>
2022-10-11 10:42:17 +0900

    add

--- index.js
+++ index.js
@@ -7,9 +7,21 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/dogs', (req, res, next) => {
-    console.log('I love dogs!')
-    next();
+const verifyPassword = ((req, res, next) => {
+    const { password } = req.query;
+    console.log(req.path)
+    console.log(password)
+    console.log(req.query)
+    if (password === "1234") {
+        next();
+    }
+    else {
+        res.send('SORRY YOU NEED A PASSWORD')
+    }
+})
+
+app.use('/secret', verifyPassword, (req, res, next) => {
+    res.send('My Secret is : Nonthing!')
 })
 
 app.get('/', (req, res) => {
@@ -20,14 +32,9 @@ app.get('/dogs', (req, res) => {
     res.send('HI DOG')
 })
 
-app.use((req, res, next) => {
-    const { password } = req.query;
-    if (password === "1234") {
-        next();
-    }
-    else {
-        res.send('SORRY YOU NEED A PASSWORD')
-    }
+app.use('/dogs', (req, res, next) => {
+    console.log('I love dogs!')
+    next();
 })
 
 app.use((req, res) => {
